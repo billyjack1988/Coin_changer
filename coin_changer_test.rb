353b59834759be227fb0056b977ea8fe3dab9001
billyjack1988 <billyjacktattoos@gmail.com>
2017-07-24 08:55:36 -0500

    passing test

--- coin_changer_test.rb
+++ coin_changer_test.rb
@@ -15,5 +15,25 @@ class Coin_changer_test < Minitest::Test
     def test_for_nickel 
         assert_equal({quater: 0, dime: 0, nickel: 1, penny: 0}, coin(5))
     end
+
+    def test_for_dime 
+        assert_equal({quater: 0, dime: 1, nickel: 0, penny: 0}, coin(10))
+    end
+
+    def test_for_quater 
+        assert_equal({quater: 1, dime: 0, nickel: 0, penny: 0}, coin(25))
+    end
+
+    def test_for_75_cents 
+        assert_equal({quater: 3, dime: 0, nickel: 0, penny: 0}, coin(75))
+    end 
+
+    def test_for_automatic_changer
+        assert_equal({quater: 25}, coin_2(25))
+    end
+
+    def test_for_78
+        assert_equal({quater: 25, penny: 1}, coin_2(26))
+    end
     
 end
